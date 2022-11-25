1fee835d6235fd5d56522abea6f72ee6e0f67554
Prashantbag <prashant.bag14@gmail.com>
2022-11-25 19:27:06 +0530

    new branch

--- PasswordManager/src/main/java/com/ty/password_mnger/controller/ConfigController.java
+++ PasswordManager/src/main/java/com/ty/password_mnger/controller/ConfigController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.ty.password_mnger.dto.UserSocial;
-import com.ty.password_mnger.service.StudentService;
 
 
 import com.ty.password_mnger.service.UserService;
@@ -74,5 +73,51 @@ public class ConfigController {
 		modelAndView.setViewName("login.jsp");
 		return modelAndView;
 	}	
+	
+	@RequestMapping("addusercredentials")
+	public ModelAndView getuserCredential(UserSocial usersocial) {
+			ModelAndView modelAndView = new ModelAndView();
+			modelAndView.addObject("usersocial",new UserSocial());
+			modelAndView.setViewName("addusercredentials.jsp");
+			
+			return modelAndView;
+	}
+	
+	
+	@RequestMapping("saveusersocial")
+	public ModelAndView savesocial(@ModelAttribute UserSocial usersocial) {
+		ModelAndView modelAndView = new ModelAndView();
+		userService.saveUserSocial(usersocial);
+		modelAndView.setViewName("index.jsp");
+		modelAndView.addObject("usersocial",new UserSocial());
+		
+		return modelAndView;
+	}
+	
+
+	@RequestMapping("login")
+	public ModelAndView login() {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("user", new User());
+		modelAndView.setViewName("login.jsp");
+
+		return modelAndView;
+	}
+
+	@RequestMapping("loginuser")
+	public ModelAndView loginUser(@ModelAttribute User user) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.addObject("user", new User());
+		
+		User user1 = userService.getUserByEmail(user);
+		System.out.println(user1.getEmail());
+		System.out.println(user1.getPassword());
+		if (user1 != null) {
+			modelAndView.setViewName("view.jsp");
+		} else {
+			modelAndView.setViewName("login.jsp");
+		}
+		return modelAndView;
+	}
 
 }
